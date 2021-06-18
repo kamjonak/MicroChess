fe05a62e94033cfd5cdeec1f92f89153e8ffc74a
Kamil <kamil.jonak18@gmail.com>
2021-06-18 18:30:24 +0200

    Duct tape logging in

--- microservices/front/front.js
+++ microservices/front/front.js
@@ -1,59 +1,115 @@
-'use strict';
+var express            = require("express"),
+ app                   = express(),
+ mongoose              = require("mongoose"),
+ bodyParser            = require('body-parser'),
+ User                  = require("./models/user"),
+ passport              = require("passport"),
+ LocalStrategy         = require("passport-local"),
+ passportLocalMongoose = require("passport-local-mongoose");
 
-const express = require('express');
-var cors = require('cors');
-const path = require('path');
-const axios = require('axios');
+ const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  }
 
-const PORT = 80;
-const HOST = '0.0.0.0';
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect("mongodb://mongo:27017/test", options).then(()=>{
+    console.log('MongoDB is connected')
+  }).catch(err=>{
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+    setTimeout(connectWithRetry, 5000)
+  })
+}
 
-// App
-const app = express();
+connectWithRetry()
+// mongoose.connect("mongodb://localhost/Samp_data");
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile); 
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cors())
+//app.use(express.static('public'));
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "once again Rusty wins cutest dogs!",
+    resave: false,
+    saveUninitialized: false
+}));
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.get('/send', (req, res) => {
-    console.log('sending to middle');
-    console.log(req.query.content);
-    axios
-        .post('http://middle:9000/send/', {
-            query: req.query.content
-        })
-        .then(function (response) {
-            console.log("response");
-        })
-        .catch(function (error) {
-            console.log("error");
-        }); 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
 
-    res.send('ok');
-});
 
-app.get('/read', (req, res) => {
-    console.log('reading from middle');
-    axios
-        .post('http://middle:9000/read/', {
-            query: req.query.content
-        })
-        .then(function (response) {
-            console.log("response:");
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(function (error) {
-            console.log("error");
-        }); 
-});
+app.get("/" ,function(req, res) {
+        res.render("home");
+     })
+     app.get("/secret",isLoggedIn, function(req, res){
+        res.render("secret")
+    })
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+
+
+app.get("/register", (req,res) => res.render("register")); 
+
+app.post("/register", (req,res)=> {
+     req.body.username
+     req.body.password
+  
+      User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/login");
+        });     
+     });
+ });
+
+//LOGIN ROUTES
+//render login form
+ app.get("/login", function(req,res){
+     res.render("login");
+ });
+ //login logic 
+ //middleware logic
+ app.post("/login", passport.authenticate("local",{
+        successRedirect:"/secret",
+        failureRedirect:"/register" }), 
+        function(req,res){      
+
+
+ });
+
+ app.get("/logout", function(req, res){
+     req.logout();
+     res.redirect("/login");
+ });
+  
+ function isLoggedIn(req, res,next){
+     if(req.isAuthenticated()){
+         return next();
+     }
+     res.redirect("/login")
+ }
+
+
+// Local-login-port;
+app.listen(7500, function() {
+    console.log("our server has been started")
+})
---
+++ microservices/front/models/user.js
@@ -0,0 +1,17 @@
+let mongoose = require("mongoose");
+let passportLocalMongoose = require("passport-local-mongoose");
+
+let UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User",UserSchema);
+
+
+
+
+
