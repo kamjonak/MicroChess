5780e2a00664ff77811b966aeddd39b9a8757be8
Kamil <kamil.jonak18@gmail.com>
2021-07-06 10:24:21 +0200

    response time decreased

--- microservices/front/routes/index.js
+++ microservices/front/routes/index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 var amqp = require('amqplib/callback_api');
 var send_channel;
 
-var resend_querry = 5000;
+var resend_querry = 100;
 
 
 function connect_to_rabbit() {
